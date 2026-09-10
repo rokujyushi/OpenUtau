a255be420d8a211215503cd1d097218a12ffc2d6
Anjo <87346264+AnAndroNerd@users.noreply.github.com>
2026-09-09 19:37:52 -0700

    1 (#2386)

--- OpenUtau.Core/Ustx/UTrack.cs
+++ OpenUtau.Core/Ustx/UTrack.cs
@@ -176,6 +176,7 @@ namespace OpenUtau.Core.Ustx {
                     var colors = Singer.Subbanks.Select(subbank => subbank.Color).ToHashSet();
                     VoiceColorExp.options = colors.OrderBy(c => c).ToArray();
                     VoiceColorExp.max = VoiceColorExp.options.Length - 1;
+                    VoiceColorExp.CustomDefaultValue = Math.Clamp(VoiceColorExp.CustomDefaultValue, VoiceColorExp.min, VoiceColorExp.max);
                 }
             }
             if (project.expressions.TryGetValue(Format.Ustx.CLRY, out var descriptor2)) {
@@ -184,6 +185,7 @@ namespace OpenUtau.Core.Ustx {
                     var colors = Singer.Subbanks.Select(subbank => subbank.Color).ToHashSet();
                     VoiceColor2Exp.options = colors.OrderBy(c => c).ToArray();
                     VoiceColor2Exp.max = VoiceColor2Exp.options.Length - 1;
+                    VoiceColor2Exp.CustomDefaultValue = Math.Clamp(VoiceColor2Exp.CustomDefaultValue, VoiceColor2Exp.min, VoiceColor2Exp.max);
                 }
             }
         }
